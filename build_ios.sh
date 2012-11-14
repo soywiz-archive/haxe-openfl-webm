67c60b8753a6874045a24b734816aa1b10775eea
soywiz <soywiz@gmail.com>
2012-11-14 22:19:07 +0100

    - More Work

--- build_ios.sh
+++ build_ios.sh
@@ -1,5 +1,5 @@
 mkdir ndll/iPhone 2> /dev/null
 pushd project
-IPHONE_VER=5.1 haxelib run hxcpp Build.xml -Diphonesim
-IPHONE_VER=5.1 haxelib run hxcpp Build.xml -Diphoneos
+IPHONE_VER=6.0 haxelib run hxcpp Build.xml -Diphonesim
+IPHONE_VER=6.0 haxelib run hxcpp Build.xml -Diphoneos
 popd
---
+++ clean.sh
@@ -0,0 +1,3 @@
+rm -rf project/obj 2> /dev/null
+rm -rf project/all_objs 2> /dev/null
+rm -rf project/vc110.pdb 2> /dev/null
